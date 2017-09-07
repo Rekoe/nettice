a3ea66bc134a6145dad0f2aee3bb413856f6920b
kouxian <kouxian@192.168.3.208>
2017-09-07 16:12:30 +0800

    add debug print

--- nettice-core/src/main/java/com/cyfonly/nettice/core/ActionDispatcher.java
+++ nettice-core/src/main/java/com/cyfonly/nettice/core/ActionDispatcher.java
@@ -1,5 +1,6 @@
 package com.cyfonly.nettice.core;
 
+import org.nutz.lang.Strings;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
 
@@ -21,101 +22,105 @@ import io.netty.util.ReferenceCountUtil;
 /**
  * Action 分发器，收到 request 后不做实际业务处理，而是组装 action 并交给处理。
  */
-public class ActionDispatcher extends ChannelInboundHandlerAdapter{
-	
+public class ActionDispatcher extends ChannelInboundHandlerAdapter {
+
 	private final static Log log = Logs.get();
-	
+
 	private static final String CONNECTION_KEEP_ALIVE = "keep-alive";
-    private static final String CONNECTION_CLOSE = "close";
-    private static final String ACTION_SUFFIX = ".action";
-	
+	private static final String CONNECTION_CLOSE = "close";
+	private static final String ACTION_SUFFIX = ".action";
+
 	protected static RouterContext routerContext;
 	private HttpRequest request;
 	private FullHttpResponse response;
 	private Channel channel;
-	
-	public ActionDispatcher(){
-		
+
+	public ActionDispatcher() {
+
 	}
-	
-	public void init(String configFilePath) throws Exception{
-		if(configFilePath == null){
+
+	public void init(String configFilePath, String suffix) throws Exception {
+		if (configFilePath == null) {
 			configFilePath = "router.xml";
 		}
-		routerContext = new RouterContext(configFilePath);
+		if (Strings.isBlank(suffix)) {
+			routerContext = new RouterContext(configFilePath);
+		} else {
+			routerContext = new RouterContext(configFilePath, suffix);
+		}
 	}
-	
+
 	@Override
-    public void channelReadComplete(ChannelHandlerContext ctx) {
-        ctx.flush();
-    }
-	
+	public void channelReadComplete(ChannelHandlerContext ctx) {
+		ctx.flush();
+	}
+
 	@Override
-	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception{
-		if(msg instanceof HttpRequest){
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		if (msg instanceof HttpRequest) {
 			channel = ctx.channel();
 			request = (HttpRequest) msg;
-			try{
+			try {
 				String path = getRequestPath();
 				ActionWrapper actionWrapper = routerContext.getActionWrapper(path);
-				if(actionWrapper == null){
+				if (actionWrapper == null) {
 					response = HttpRenderUtil.getNotFoundResponse();
 					writeResponse(true);
+					 if (log.isDebugEnabled())
+				            log.debugf("Search mapping path=%s : NOT Action match", path);
 					return;
 				}
 				DataHolder.setRequest(request);
 				ActionProxy proxy = routerContext.getActionProxy(actionWrapper);
 				Return result = proxy.execute();
-				if(result != null){
+				if (result != null) {
 					response = result.process();
 				}
 				writeResponse(false);
-			}catch(Exception e){
-				log.error(e);
+			} catch (Exception e) {
 				response = HttpRenderUtil.getErroResponse();
 				writeResponse(true);
-			}finally{
+			} finally {
 				ReferenceCountUtil.release(msg);
 			}
 		}
 	}
-	
+
 	@Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
-        ctx.close();
-    }
-	
-	private String getRequestPath() throws Exception{
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+	private String getRequestPath() throws Exception {
 		String uri = request.uri();
-		if("/favicon.ico".equals(uri)) {
+		if ("/favicon.ico".equals(uri)) {
 			return uri;
 		}
 		int startIndex = uri.indexOf(ACTION_SUFFIX);
-		if(startIndex <= 0){
+		if (startIndex <= 0) {
 			throw new Exception("request path error");
 		}
-		return uri.substring(0,startIndex + ACTION_SUFFIX.length());
+		return uri.substring(0, startIndex + ACTION_SUFFIX.length());
 	}
+
 	private static final AsciiString CONTENT_LENGTH = new AsciiString("Content-Length");
-	
-	private void writeResponse(boolean forceClose){
+
+	private void writeResponse(boolean forceClose) {
 		boolean close = isClose();
-		if(!close && !forceClose){
+		if (!close && !forceClose) {
 			response.headers().add(CONTENT_LENGTH, String.valueOf(response.content().readableBytes()));
 		}
 		ChannelFuture future = channel.write(response);
-		if(close || forceClose){
+		if (close || forceClose) {
 			future.addListener(ChannelFutureListener.CLOSE);
 		}
 	}
-	
-	private boolean isClose(){
-		if(request.headers().contains(CONTENT_LENGTH, CONNECTION_CLOSE, true) ||
-				(request.protocolVersion().equals(HttpVersion.HTTP_1_0) && 
-				!request.headers().contains(CONTENT_LENGTH, CONNECTION_KEEP_ALIVE, true)))
+
+	private boolean isClose() {
+		if (request.headers().contains(CONTENT_LENGTH, CONNECTION_CLOSE, true) || (request.protocolVersion().equals(HttpVersion.HTTP_1_0) && !request.headers().contains(CONTENT_LENGTH, CONNECTION_KEEP_ALIVE, true)))
 			return true;
 		return false;
 	}
-	
+
 }
--- nettice-examples/src/main/java/com/cyfonly/nettice/examples/HttpServer.java
+++ nettice-examples/src/main/java/com/cyfonly/nettice/examples/HttpServer.java
@@ -17,45 +17,39 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 
 public class HttpServer {
-	
+
 	private static final Log logger = Logs.get();
-	
+
 	private final int port;
-	public HttpServer(int port){
+
+	public HttpServer(int port) {
 		this.port = port;
 	}
-	
-	public void run() throws Exception{
+
+	public void run() throws Exception {
 		EventLoopGroup bossGroup = new NioEventLoopGroup();
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
-		
-		try{
+
+		try {
 			ServerBootstrap bootstrap = new ServerBootstrap();
-			bootstrap.group(bossGroup, workerGroup)
-				.channel(NioServerSocketChannel.class)
-				.childHandler(new ChannelInitializer<SocketChannel>() {
-	                @Override
-	                public void initChannel(SocketChannel ch) throws Exception {
-	                   ch.pipeline().addLast("codec",new HttpServerCodec())
-	                   				.addLast("aggregator",new HttpObjectAggregator(1024*1024))
-	                   				.addLast("dispatcher",new ActionDispatcher());
-	                }
-	            })
-	            .option(ChannelOption.SO_BACKLOG, 1024)
-	            .childOption(ChannelOption.SO_KEEPALIVE, true)
-	            .childOption(ChannelOption.TCP_NODELAY, true);
+			bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).childHandler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				public void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast("codec", new HttpServerCodec()).addLast("aggregator", new HttpObjectAggregator(1024 * 1024)).addLast("dispatcher", new ActionDispatcher());
+				}
+			}).option(ChannelOption.SO_BACKLOG, 1024).childOption(ChannelOption.SO_KEEPALIVE, true).childOption(ChannelOption.TCP_NODELAY, true);
 			ChannelFuture future = bootstrap.bind(port).sync();
 			logger.info("Nettp server listening on port " + port);
 			future.channel().closeFuture().sync();
-		}finally{
+		} finally {
 			bossGroup.shutdownGracefully();
 			workerGroup.shutdownGracefully();
 		}
 	}
-	
-	public static void main(String[] args) throws Exception{
+
+	public static void main(String[] args) throws Exception {
 		ActionDispatcher dispatcher = new ActionDispatcher();
-		dispatcher.init("nettice.xml");
+		dispatcher.init("nettice.xml", "om");
 		new HttpServer(8080).run();
 	}
 
